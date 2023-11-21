36c756983421d01bb23f8b68a5c65ef722e742ee
albertomandirola <albymandi00@gmail.com>
2023-11-21 10:34:01 +0100

    ex . 2

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let comparison = "le stringhe sono uguali";
+let string1 = "lropem impès doadkm"
+let string2 = "lropem impès doadk"
+
+if (string1 == string2){
+    console.log(comparison)
+}
+else{
+    comparison = "le stringhe sono diverse"
+    console.log(comparison)
+}
