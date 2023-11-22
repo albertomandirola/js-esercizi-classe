25435eb096611ab337cd5d1be317e111aaf28b6c
albertomandirola <albymandi00@gmail.com>
2023-11-22 10:31:41 +0100

    added es 3

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 **Esercizio 2**
 Assegna alla variabile `comparison` il risultato del confronto tra due stringhe per verificare se le due stringhe sono uguali. Stampa sulla console il valore di `comparison`
+
+**Esercizio 3**
+Assegna alla variabile `comparison` il risultato del confronto tra un numero e una stringa (che rappresenta un numero) per verificare se sono uguali. Stampa sulla console il valore di `comparison`
--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-let comparison = "le stringhe sono uguali";
+//esercizio 2
+/* let comparison = "le stringhe sono uguali";
 let string1 = "lropem impès doadkm"
 let string2 = "lropem impès doadk"
 
@@ -8,4 +9,12 @@ if (string1 == string2){
 else{
     comparison = "le stringhe sono diverse"
     console.log(comparison)
+} */
+//eserczio 3
+let comparison = true;
+let string1 = "3"
+let num1 = 3
+if (string1 !== num1){
+    comparison = false
 }
+console.log(comparison)
